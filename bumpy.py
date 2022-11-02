a5c65712b91d01c302a43fb4fc8a807b6272031a
Dillon McCarthy <47464223+dillonrmccarthy@users.noreply.github.com>
2022-11-02 15:55:50 -0400

    Create bumpy.py

---
+++ bumpy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import numpy as np
+
+bumpy = np.random.randint(10,size=(5)) # 1D array
+bumpy_a0 = np.expand_dims(bumpy, axis=0) #shape is now (1,5)
+print(bumpy_a0.shape) 
+bumpy_a0T = bumpy_a0.T #this makes shape (5,1)
+print(bumpy_a0T.shape) 
+
+#ORRR YOU COULD BE SMART AND NOT A FUCKING MORON LIKE I AM AND JUST SWAP THE AXIS
+bumpy_a1 = np.expand_dims(bumpy, axis=1) #shape is now (5,5)
+print(bumpy_a0T.shape == bumpy_a1.shape)
+
+exikt
